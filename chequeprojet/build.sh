90c39a4f0e15b524cce01d0f57997d1cae55c4b6
Jyothirmai <chennujyothirmai@gmail.com>
2026-04-03 20:20:44 +0530

    Fix 500 Internal Server Error: Add database migrations to build script

--- chequeprojet/build.sh
+++ chequeprojet/build.sh
@@ -2,5 +2,9 @@
 # Install dependencies
 pip install -r requirements.txt
 
+# Run database migrations (IMPORTANT for fixing Internal Server Error)
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
 # Create static files
 python manage.py collectstatic --noinput

260f9914919e78a01a9d7489a6fd49b1785a65d6
Jyothirmai <chennujyothirmai@gmail.com>
2026-04-03 18:54:04 +0530

    Configure mobile app to use live Render URL

--- cheque_mobile/App.js
+++ cheque_mobile/App.js
@@ -4,15 +4,16 @@ import { WebView } from 'react-native-webview';
 
 export default function App() {
   const DJANGO_SERVER_URL = Platform.select({
-    web: 'http://localhost:8000',
-    default: 'http://10.76.10.85:8000',
+    web: 'https://cheque-app-7cem.onrender.com',
+    default: 'https://cheque-app-7cem.onrender.com',
   });
+
 
   if (Platform.OS === 'web') {
     return (
       <div style={{ flex: 1, height: '100vh', width: '100vw', margin: 0, padding: 0, overflow: 'hidden' }}>
-        <iframe 
-          src={DJANGO_SERVER_URL} 
+        <iframe
+          src={DJANGO_SERVER_URL}
           style={{ width: '100%', height: '100%', border: 'none' }}
           title="Cheque System"
         />
